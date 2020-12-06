a6702a69cd61de7ab0b10aa3f809e04c880386c2
rodrigo-garay95 <garay95@live.com>
2020-12-05 23:12:49 -0300

    api de bicicletas

--- routes/api/bicicletas.js
+++ routes/api/bicicletas.js
@@ -4,6 +4,7 @@ var bicicletaController = require('../../controllers/api/bicicletaControllerAPI'
 
 router.get('/', bicicletaController.bicicleta_list);
 router.post('/create', bicicletaController.bicicleta_create);
+router.post('/:id/update', bicicletaController.bicicleta_update);
 router.delete('/delete', bicicletaController.bicicleta_delete);
 
 module.exports = router;
